f6625cbf99743580161f22590a60a5f467036ea7
srothfleisch <91762507+srothfleisch@users.noreply.github.com>
2023-10-04 14:02:19 -0500

    mostCommonClass and ID3 base cases
    Co-authored-by: Elray326 <Elray326@users.noreply.github.com>

--- HW1/ID3.py
+++ HW1/ID3.py
@@ -1,7 +1,39 @@
 from node import Node
+from parse import parse
 import math
 
+# finds the most common class, and if the common class equals the total
+def mostCommonClass(examples):
+  neg = 0
+  pos = 0
+  total = 0
+  for d in examples:
+    total += 1
+    if d["Class"] == '0':
+      neg += 1
+    else:
+      pos += 1
+  # if empty, return empty node t
+  if total == 0:
+    return [None, True]
+  if pos - neg >= 0:
+    return ['1', pos == total]
+  return ['0', neg == total]
+
+def informationGain(examples):
+  pass
+
 def ID3(examples, default):
+  # create initial node
+  t = Node()
+  # find common class
+  t.label, result = mostCommonClass(examples)
+  if result is True:  # if all examples in D are positive or negative
+    return t
+  # continue with algorithm
+  else:
+    Astar = informationGain(examples)
+  
   '''
   Takes in an array of examples, and returns a tree (an instance of Node) 
   trained on the examples.  Each example is a dictionary of attribute:value pairs,
@@ -27,3 +59,6 @@ def evaluate(node, example):
   Takes in a tree and one example.  Returns the Class value that the tree
   assigns to the example.
   '''
+
+print(parse("HW1/candy.data"))
+print(mostCommonClass(parse("HW1/tennis.data")))
