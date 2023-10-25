a1be2a7411b35a2a895fa21f548b7bf9e71edae2
Elray326 <elray326@gmail.com>
2023-10-25 13:37:53 -0500

    finished Q1

--- HW2/starter.py
+++ HW2/starter.py
@@ -1,12 +1,55 @@
+import numpy as np
+from numpy.linalg import norm
+
 # returns Euclidean distance between vectors a dn b
 def euclidean(a,b):
-    
+    dist = 0
+    for i in range(len(a)):
+        dist += (a[i] - b[i]) ** 2
+    dist = dist ** 0.5
     return(dist)
-        
+
+"""
+# Euclidian distance test
+point1 = np.array((1, 2, 3, 11, 22, 3, 1, -155))
+point2 = np.array((1, 1, 1, 5, 3, 4, 10, 8))
+ 
+# calculating Euclidean distance
+# using linalg.norm()
+dist_ref = np.linalg.norm(point1 - point2)
+dist = euclidean(point1, point2)
+
+# printing Euclidean distance
+print("Reference:",dist_ref)
+print("Ours:",dist)
+"""
+
 # returns Cosine Similarity between vectors a dn b
 def cosim(a,b):
     
+    dist = sum(a * b) / (vecSumSqrt(a) * vecSumSqrt(b))
+
     return(dist)
+
+# gets the square root of the sum of the vector
+def vecSumSqrt(vec):
+    dist = 0
+    for i in range(len(vec)):
+        dist += vec[i] ** 2
+    return dist ** 0.5
+
+"""
+# define two lists or array
+A = np.array([2,1,2,3,2,9])
+B = np.array([3,4,2,4,5,5])
+
+ 
+# compute cosine similarity
+ref_cosine = np.dot(A,B)/(norm(A)*norm(B))
+cosine = cosim(A,B)
+print("Reference Cosine Similarity:", ref_cosine)
+print("Cosine Similarity:", cosine)
+"""
 
 # returns a list of labels for the query dataset based upon labeled observations in the train dataset.
 # metric is a string specifying either "euclidean" or "cosim".  
@@ -57,4 +100,4 @@ def main():
     
 if __name__ == "__main__":
     main()
-    
+#"""
